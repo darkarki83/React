f419464124009dd6c65cbe3625eede88613fe4be
Artem_Krol <artemkrol47@gmail.com>
2021-08-06 16:43:38 +0300

    add moduls

---
+++ 01_first_project/my-app/src/Header.js
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">News Feed</a>
+      <a href="#">Massages</a>
+    </div>
+  );
+}
+
+export default Header;
---
+++ 01_first_project/my-app/src/Tehnologies.js
@@ -0,0 +1,21 @@
+
+
+const Tehnologies = () => {
+    return (
+      <div>
+         <ul>
+          <li>
+            CSS
+          </li>
+          <li>
+            HTML
+          </li>
+          <li>
+            React
+          </li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Tehnologies;
