5b4c8ddbc5a685509492c196f428eb132ca0faa2
Artem_Krol <artemkrol47@gmail.com>
2021-08-08 00:37:32 +0300

    lisson 12 finish start component in social network

--- 01_first_project/my-app/src/App.js
+++ 01_first_project/my-app/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
-    <div >
-      <header>
-        
-      </header>
-
-      <h1 className="App">ArtKDev</h1>
- 
+    <div className="app-wrapper" >
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
