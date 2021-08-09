69fb57fa08b5a68a25ee2eb23ad12e456eaa6b7e
Artem_Krol <artemkrol47@gmail.com>
2021-08-09 14:56:48 +0300

    18 lesson 188 add new module Dialog

--- 01_first_project/my-app/src/components/Profile/Profile.jsx
+++ 01_first_project/my-app/src/components/Profile/Profile.jsx
@@ -3,17 +3,17 @@ import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css';
 
 const Profile = () => {
-    return (
-        <div className={ s.content }>
-        <div>
-          <img src="https://www.imgonline.com.ua/examples/rainbow-background-1-big.jpg" />
-        </div>
-        <div>
-          ava + description
-        </div>
-      <MyPosts hey="yo"/>
+  return (
+    <div>
+      <div>
+        <img src="https://www.imgonline.com.ua/examples/rainbow-background-1-big.jpg" />
       </div>
-    );
+      <div>
+        ava + description
+      </div>
+      <MyPosts />
+    </div>
+  );
 }
 
 export default Profile;
