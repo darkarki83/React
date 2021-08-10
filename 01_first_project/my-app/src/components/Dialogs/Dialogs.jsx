dadd79e104eaa2dccbe705d7508c5c947aabbe85
Artem_Krol <artemkrol47@gmail.com>
2021-08-10 15:52:04 +0300

    21 lesson finish update dialog page

--- 01_first_project/my-app/src/components/Dialogs/Dialogs.jsx
+++ 01_first_project/my-app/src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,30 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-      <div>
-          Dialogs
+      <div className={s.dialogs}>
+          <div className={s.dialogsItems}>
+            <div className={s.dialog + ' ' + s.active}>
+              Katia
+            </div>
+            <div className={s.dialog}>
+              Vova
+            </div>
+            <div className={s.dialog}>
+              Sasha
+            </div>
+            <div className={s.dialog}>
+              Pasha
+            </div>
+            <div className={s.dialog}>
+              Nikita
+            </div>
+          </div>
+          <div className={s.messages}>
+            <div className={s.message}>Hi</div>
+            <div className={s.message}>How are you</div>
+            <div className={s.message}>Hi artkdev</div>
+            <div className={s.message}>yo yo</div>
+          </div>
       </div>
     );
 }
