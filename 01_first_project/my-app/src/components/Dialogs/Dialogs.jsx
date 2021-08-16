5ddc880266afd9342d5e8fa498eae981891bc16c
Artem_Krol <artemkrol47@gmail.com>
2021-08-17 00:58:09 +0300

    31 hw add to dialog textarea

--- 01_first_project/my-app/src/components/Dialogs/Dialogs.jsx
+++ 01_first_project/my-app/src/components/Dialogs/Dialogs.jsx
@@ -4,6 +4,14 @@ import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
 
 const Dialogs = (props) => {
+
+  let messageElement = React.createRef();
+
+  const onButtonClick = () => {
+    let mess = messageElement.current.value;
+    console.log(mess)
+  }
+
   let dialogsElements = props.state.dialogsData.map( (d) => {
     return <DialogItem id={d.id} name={d.name} />
   });
@@ -18,6 +26,8 @@ const Dialogs = (props) => {
       <div className={s.messages}>
         { messagesElements }
       </div>
+      <textarea ref={messageElement}></textarea>
+      <button onClick={onButtonClick}>add message</button>
     </div>
   );
 }
